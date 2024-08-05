841e2902d8463a7bd62a53e6b322fe2491a3a2ae
Gabriel Grün <gabrielg737@gmail.com>
2024-08-04 22:36:20 -0300

    Temporary user login OK

---
+++ frontend/js/login.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    bindBtnEntrar();
+}
+
+function bindBtnEntrar() {
+    document.querySelector(`#btnEntrar`).addEventListener('click', doLogin);
+}
+
+async function doLogin(e) {
+    e.preventDefault();
+    const login = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+    const data = {
+        email: login,
+        senha: password
+    };
+
+    try {
+        const response = await fetch(`/api/usuarios/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            const alertContainer = document.getElementById('alertContainer');
+            alertContainer.innerHTML = `
+                    <div class="alert alert-danger" role="alert">
+                        Erro ao realizar login. Por favor, verifique suas credenciais e tente novamente.
+                    </div>
+                `;
+        }
+
+        const result = await response.json();
+        localStorage.setItem('userToken', result.token);
+        window.location.href = '/index.html';
+    } catch (error) {   
+        const alertContainer = document.getElementById('alertContainer');
+        return alertContainer.innerHTML = `
+        <div class="alert alert-danger" role="alert">
+            Erro ao realizar login. Por favor, verifique suas credenciais e tente novamente.
+        </div>
+    `;
+    }
+}
